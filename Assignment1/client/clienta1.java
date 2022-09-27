5109e64045d55cd11072ba2121ef2733a796a677
Cwagne17 <cwagne17@students.towson.edu>
2022-09-27 19:15:34 -0400

    Code is finished

--- Assignment1/client/clienta1.java
+++ Assignment1/client/clienta1.java
@@ -1,32 +1,43 @@
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.math.BigInteger;
-import java.io.Console;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.util.*;
-import java.util.logging.*;
-
-import java.net.InetSocketAddress;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
-import java.net.SocketAddress;
-
-import java.security.*;
-import java.security.spec.ECParameterSpec;
-import java.security.spec.EncodedKeySpec;
-import java.security.spec.X509EncodedKeySpec;
-import java.security.interfaces.ECPublicKey;
-
-import javax.crypto.KeyAgreement;
-
 import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.spec.EncodedKeySpec;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyAgreement;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 public class clienta1 {
+
+    private static final String ENCRYPT_ALGO = "AES/GCM/NoPadding";
+    private static final int TAG_LENGTH_BIT = 128;
+    private static final int IV_LENGTH_BYTE = 12;
+
+    private static final Charset UTF_8 = StandardCharsets.UTF_8;
 
     // Utils
     private final static Logger logger = Logger.getAnonymousLogger();
@@ -39,55 +50,130 @@ public class clienta1 {
                 logger.log(Level.SEVERE, "Can't connect to socket");
                 System.exit(1);
             }
-            logger.log(Level.INFO, "Client connected at " + socket.getLocalPort());
-
-            DataInputStream input = new DataInputStream(socket.getInputStream());
+            logger.log(Level.INFO, "Client connected to " + socket.getInetAddress() + ":" + socket.getPort());
+            
             DataOutputStream output = new DataOutputStream(socket.getOutputStream());
-            
-            // Generate Client Keys & Send Public Key
-            KeyPairGenerator kpg = KeyPairGenerator.getInstance("EC");
-            kpg.initialize(256);
-            KeyPair kp = kpg.generateKeyPair();;
-            output.write(kp.getPublic().getEncoded());
-            
-            // Read and construct server public key
-            byte[] buffer = new byte[91];
-            input.read(buffer);
-            KeyFactory kf = KeyFactory.getInstance("EC");
-            EncodedKeySpec pkSpec = new X509EncodedKeySpec(buffer);
-            PublicKey server_pk = kf.generatePublic(pkSpec);
+            DataInputStream input = new DataInputStream(socket.getInputStream());
 
-            // Perform key agreement
-            KeyAgreement ka = KeyAgreement.getInstance("ECDH");
-            ka.init(kp.getPrivate());
-            ka.doPhase(server_pk, true);
-            byte[] sharedSecret = ka.generateSecret();
-            System.out.println("Shared Secret: " + sharedSecret);
-            
-            // Derive a key from the shared secret and both public keys
-            MessageDigest hash = MessageDigest.getInstance("SHA-256");
-            hash.update(sharedSecret);
-            List<ByteBuffer> keys = Arrays.asList(ByteBuffer.wrap(kp.getPublic().getEncoded()), ByteBuffer.wrap(server_pk.getEncoded()));
-            Collections.sort(keys);
-            hash.update(keys.get(0));
-            hash.update(keys.get(1));
+            // ECDH key exchange
+            KeyPair client_key_pair = generateKeyPair(output);
+            PublicKey server_pk = readServerKey(input);
+            byte[] derivedKey = getDerivedKey(server_pk, client_key_pair);
 
-            byte[] derivedKey = hash.digest();
-            BigInteger bigint = new BigInteger(derivedKey);
-            System.out.printf("Final key: %s%n", bigint.toString(16));
+            // Get user input
+            String plaintext = getClientInput();
 
-            Scanner scan = new Scanner(System.in);
-            System.out.println("Enter a short message:");
-            String message = scan.nextLine();
-            scan.close();
-            System.out.println(message);
+            // encrypt and decrypt need the same key.
+            // get AES 256 bits (32 bytes) key
+            SecretKey secretKey = new SecretKeySpec(derivedKey, 0, derivedKey.length, "AES");
+
+            // encrypt and decrypt need the same IV.
+            // AES-GCM needs IV 96-bit (12 bytes)
+            byte[] iv = getRandomNonce(IV_LENGTH_BYTE);
+            byte[] encryptedText = encryptWithPrefixIV(plaintext.getBytes(UTF_8), secretKey, iv);
+
+            String OUTPUT_FORMAT = "%-30s:%s";
+            System.out.println("\n------ AES GCM Encryption ------");
+            System.out.println(String.format(OUTPUT_FORMAT, "Input (plain text)", plaintext));
+            System.out.println(String.format(OUTPUT_FORMAT, "Derived Key (hex)", hex(secretKey.getEncoded())));
+            System.out.println(String.format(OUTPUT_FORMAT, "IV  (hex)", hex(iv)));
+            System.out.println(String.format(OUTPUT_FORMAT, "Encrypted (hex) ", hex(encryptedText)));
+
+            output.write(encryptedText);
 
             // Closes all connections
             output.close();
-            input.close();
             socket.close();
         } catch (Exception exception) {
             exception.printStackTrace();
         }
     }
+
+    /**
+     * ECDH Key Exchange
+     */
+
+    // Generate Client Keys & Send Public Key
+    public static KeyPair generateKeyPair(DataOutputStream output) throws IOException, NoSuchAlgorithmException {
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("EC");
+        kpg.initialize(256);
+        KeyPair kp = kpg.generateKeyPair();
+        output.write(kp.getPublic().getEncoded());
+        return kp;
+    }
+
+    // Read and construct server public key
+    public static PublicKey readServerKey(InputStream input) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        byte[] buffer = new byte[91];
+        input.read(buffer);
+
+        KeyFactory kf = KeyFactory.getInstance("EC");
+        EncodedKeySpec pkSpec = new X509EncodedKeySpec(buffer);
+        PublicKey server_pk = kf.generatePublic(pkSpec);
+        return server_pk;
+    }
+
+    public static byte[] getDerivedKey(PublicKey serverPublicKey, KeyPair clientKeyPair) throws NoSuchAlgorithmException, InvalidKeyException, IllegalStateException {
+        // Perform key agreement
+        KeyAgreement ka = KeyAgreement.getInstance("ECDH");
+        ka.init(clientKeyPair.getPrivate());
+        ka.doPhase(serverPublicKey, true);
+        byte[] sharedSecret = ka.generateSecret();
+        
+        // Derive a key from the shared secret and both public keys
+        MessageDigest hash = MessageDigest.getInstance("SHA-256");
+        hash.update(sharedSecret);
+        List<ByteBuffer> keys = Arrays.asList(ByteBuffer.wrap(clientKeyPair.getPublic().getEncoded()), ByteBuffer.wrap(serverPublicKey.getEncoded()));
+        Collections.sort(keys);
+        hash.update(keys.get(0));
+        hash.update(keys.get(1));
+        return hash.digest();
+    }
+
+    public static String getClientInput() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\nEnter a short message:");
+        String message = scan.nextLine();
+        scan.close();
+        return message;
+    }
+
+    /**
+     * AES_GCM Encryption
+     */
+
+    public static byte[] getRandomNonce(int numBytes) {
+        byte[] nonce = new byte[numBytes];
+        new SecureRandom().nextBytes(nonce);
+        return nonce;
+    }
+
+    // hex representation
+    public static String hex(byte[] bytes) {
+        StringBuilder result = new StringBuilder();
+        for (byte b : bytes) {
+            result.append(String.format("%02x", b));
+        }
+        return result.toString();
+    }
+
+    // AES-GCM needs GCMParameterSpec
+    public static byte[] encrypt(byte[] pText, SecretKey secret, byte[] iv) throws Exception {
+        Cipher cipher = Cipher.getInstance(ENCRYPT_ALGO);
+        cipher.init(Cipher.ENCRYPT_MODE, secret, new GCMParameterSpec(TAG_LENGTH_BIT, iv));
+        byte[] encryptedText = cipher.doFinal(pText);
+        return encryptedText;
+    }
+
+    // prefix IV length + IV bytes to cipher text
+    public static byte[] encryptWithPrefixIV(byte[] pText, SecretKey secret, byte[] iv) throws Exception {
+        byte[] cipherText = encrypt(pText, secret, iv);
+
+        byte[] cipherTextWithIv = ByteBuffer.allocate(iv.length + cipherText.length)
+                .put(iv)
+                .put(cipherText)
+                .array();
+        return cipherTextWithIv;
+    }
+
 }
--- Assignment1/server/servera1.java
+++ Assignment1/server/servera1.java
@@ -1,36 +1,44 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.math.BigInteger;
-import java.io.Console;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.util.*;
-import java.util.logging.*;
-
-import java.net.InetSocketAddress;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketAddress;
-
-import java.security.*;
-import java.security.spec.ECParameterSpec;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
 import java.security.spec.EncodedKeySpec;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
-import java.security.interfaces.ECPublicKey;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+import javax.crypto.Cipher;
 import javax.crypto.KeyAgreement;
-
-import java.nio.ByteBuffer;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 
 public class servera1 {
+    // AES_GCM Variables
+    private static final String ENCRYPT_ALGO = "AES/GCM/NoPadding";
+    private static final int TAG_LENGTH_BIT = 128;
+    private static final int IV_LENGTH_BYTE = 12;
+    private static final Charset UTF_8 = StandardCharsets.UTF_8;
+
     // Utils
-    private final static Logger logger = Logger.getAnonymousLogger();
+    private final static String OUTPUT_FORMAT = "%-30s:%s";
 
     public static void main(String[] args) {
         try {
@@ -43,57 +51,103 @@ public class servera1 {
 
             // Listen for clients
             if(!server.isClosed()) {
-                logger.log(Level.INFO, "Server listening at " + server.getInetAddress());
-                while (true) {
-                    try {
-                        Socket socket = server.accept();
-                        handleClient(socket);
-                    } catch (Exception exception) {
-                        exception.getStackTrace();
-                    }
+                System.out.println("------ Server Information ------");
+                System.out.println(String.format(OUTPUT_FORMAT, "Server Port", server.getLocalPort()));
+
+                try {
+                    Socket socket = server.accept();
+                    System.out.println("\nClient connection established from " + socket.getRemoteSocketAddress());
+                    handleClient(socket);
+                } catch (Exception exception) {
+                    exception.getStackTrace();
                 }
             }
-
             server.close();
         } catch(Exception exception) {
             exception.getStackTrace();
         } 
     }
 
-    public static void handleClient(Socket conn) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException {
-        InputStream input = new DataInputStream(conn.getInputStream());
+    public static void handleClient(Socket conn) throws Exception {
         OutputStream output = new DataOutputStream(conn.getOutputStream());
+        InputStream input = new DataInputStream(conn.getInputStream());
 
         // Read and construct client public key
-        byte[] buffer = new byte[91];
-        input.read(buffer);
-        KeyFactory kf = KeyFactory.getInstance("EC");
-        EncodedKeySpec pkSpec = new X509EncodedKeySpec(buffer);
-        PublicKey client_pk = kf.generatePublic(pkSpec);
+        PublicKey client_pk = getClientPublicKey(input);
 
         // Generate Server Keys & Send Public Key
         KeyPairGenerator kpg = KeyPairGenerator.getInstance("EC");
         kpg.initialize(256);
-        KeyPair kp = kpg.generateKeyPair();
-        output.write(kp.getPublic().getEncoded());
+        KeyPair serverKeyPair = kpg.generateKeyPair();
+        output.write(serverKeyPair.getPublic().getEncoded());
 
+        byte[] derivedKey = getDerivedKey(client_pk, serverKeyPair);
+
+        byte[] encryptedText =  input.readAllBytes();
+
+        SecretKey secretKey = new SecretKeySpec(derivedKey, 0, derivedKey.length, "AES");
+
+        System.out.println("\n------ AES GCM Decryption ------");
+        System.out.println(String.format(OUTPUT_FORMAT, "Input (hex)", hex(encryptedText)));
+        System.out.println(String.format(OUTPUT_FORMAT, "Key (hex)", hex(secretKey.getEncoded())));
+
+        String decryptedText = decryptWithPrefixIV(encryptedText, secretKey);
+
+        System.out.println(String.format(OUTPUT_FORMAT, "Decrypted (plain text)", decryptedText));
+    }
+
+    public static PublicKey getClientPublicKey(InputStream input) throws IOException, InvalidKeySpecException, NoSuchAlgorithmException {
+        byte[] buffer = new byte[91];
+        input.read(buffer);
+
+        KeyFactory kf = KeyFactory.getInstance("EC");
+        EncodedKeySpec pkSpec = new X509EncodedKeySpec(buffer);
+        return kf.generatePublic(pkSpec);
+    }
+
+    public static byte[] getDerivedKey(PublicKey clientPublicKey, KeyPair serverKeyPair) throws NoSuchAlgorithmException, InvalidKeyException, IllegalStateException {
         // Perform key agreement
         KeyAgreement ka = KeyAgreement.getInstance("ECDH");
-        ka.init(kp.getPrivate());
-        ka.doPhase(client_pk, true);
+        ka.init(serverKeyPair.getPrivate());
+        ka.doPhase(clientPublicKey, true);
         byte[] sharedSecret = ka.generateSecret();
-        System.out.println("Shared Secret: " + sharedSecret);
-
+        
         // Derive a key from the shared secret and both public keys
         MessageDigest hash = MessageDigest.getInstance("SHA-256");
         hash.update(sharedSecret);
-        List<ByteBuffer> keys = Arrays.asList(ByteBuffer.wrap(kp.getPublic().getEncoded()), ByteBuffer.wrap(client_pk.getEncoded()));
+        List<ByteBuffer> keys = Arrays.asList(ByteBuffer.wrap(serverKeyPair.getPublic().getEncoded()), ByteBuffer.wrap(clientPublicKey.getEncoded()));
         Collections.sort(keys);
         hash.update(keys.get(0));
         hash.update(keys.get(1));
+        return hash.digest();
+    }
 
-        byte[] derivedKey = hash.digest();
-        BigInteger bigint = new BigInteger(derivedKey);
-        System.out.printf("Final key: %s%n", bigint.toString(16));
+    // hex representation
+    public static String hex(byte[] bytes) {
+        StringBuilder result = new StringBuilder();
+        for (byte b : bytes) {
+            result.append(String.format("%02x", b));
+        }
+        return result.toString();
+    }
+
+    public static String decrypt(byte[] cText, SecretKey secret, byte[] iv) throws Exception {
+        Cipher cipher = Cipher.getInstance(ENCRYPT_ALGO);
+        cipher.init(Cipher.DECRYPT_MODE, secret, new GCMParameterSpec(TAG_LENGTH_BIT, iv));
+        byte[] plainText = cipher.doFinal(cText);
+        return new String(plainText, UTF_8);
+    }
+
+    public static String decryptWithPrefixIV(byte[] cText, SecretKey secret) throws Exception {
+        ByteBuffer bb = ByteBuffer.wrap(cText);
+
+        byte[] iv = new byte[IV_LENGTH_BYTE];
+        bb.get(iv);
+
+        byte[] cipherText = new byte[bb.remaining()];
+        bb.get(cipherText);
+
+        String plainText = decrypt(cipherText, secret, iv);
+        return plainText;
     }
 }

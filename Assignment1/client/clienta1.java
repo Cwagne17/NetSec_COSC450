14ea48555ae93a84a3c852436997dbf5cad126e9
Cwagne17 <cwagne17@students.towson.edu>
2022-09-27 15:00:07 -0400

    add final key derivation

--- Assignment1/client/clienta1.java
+++ Assignment1/client/clienta1.java
@@ -1,10 +1,14 @@
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.io.Console;
-
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.util.*;
 import java.util.logging.*;
 
@@ -37,21 +41,20 @@ public class clienta1 {
             }
             logger.log(Level.INFO, "Client connected at " + socket.getLocalPort());
 
-
-            // Inits the I/O Objects
-            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
             
-            KeyPair kp = generateKeys();
-            output.println(kp.getPublic().getEncoded());
-
-            long val = Integer.parseInt(input.readLine(), 16);
-            byte[] server_pk_bytes = BigInteger.valueOf(val).toByteArray();
-            System.out.println("Server Public Key: " + server_pk_bytes);
-
+            // Generate Client Keys & Send Public Key
+            KeyPairGenerator kpg = KeyPairGenerator.getInstance("EC");
+            kpg.initialize(256);
+            KeyPair kp = kpg.generateKeyPair();;
+            output.write(kp.getPublic().getEncoded());
+            
+            // Read and construct server public key
+            byte[] buffer = new byte[91];
+            input.read(buffer);
             KeyFactory kf = KeyFactory.getInstance("EC");
-            EncodedKeySpec pkSpec = new X509EncodedKeySpec(server_pk_bytes);
-            System.out.println("pkspec: " + pkSpec.getEncoded());
+            EncodedKeySpec pkSpec = new X509EncodedKeySpec(buffer);
             PublicKey server_pk = kf.generatePublic(pkSpec);
 
             // Perform key agreement
@@ -60,6 +63,24 @@ public class clienta1 {
             ka.doPhase(server_pk, true);
             byte[] sharedSecret = ka.generateSecret();
             System.out.println("Shared Secret: " + sharedSecret);
+            
+            // Derive a key from the shared secret and both public keys
+            MessageDigest hash = MessageDigest.getInstance("SHA-256");
+            hash.update(sharedSecret);
+            List<ByteBuffer> keys = Arrays.asList(ByteBuffer.wrap(kp.getPublic().getEncoded()), ByteBuffer.wrap(server_pk.getEncoded()));
+            Collections.sort(keys);
+            hash.update(keys.get(0));
+            hash.update(keys.get(1));
+
+            byte[] derivedKey = hash.digest();
+            BigInteger bigint = new BigInteger(derivedKey);
+            System.out.printf("Final key: %s%n", bigint.toString(16));
+
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter a short message:");
+            String message = scan.nextLine();
+            scan.close();
+            System.out.println(message);
 
             // Closes all connections
             output.close();
@@ -68,12 +89,5 @@ public class clienta1 {
         } catch (Exception exception) {
             exception.printStackTrace();
         }
-    }
-
-    public static KeyPair generateKeys() throws NoSuchAlgorithmException {
-        // Generate ephemeral ECDH keypair
-        KeyPairGenerator kpg = KeyPairGenerator.getInstance("EC");
-        kpg.initialize(256);
-        return kpg.generateKeyPair();
     }
 }
